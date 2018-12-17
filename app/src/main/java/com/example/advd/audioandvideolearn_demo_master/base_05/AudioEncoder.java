e803d6bc0321f7b77470ceb1b3fc3e192879bc66
ycl <907740329@qq.com>
2018-12-17 18:04:36 +0800

    完成音频编码AudioEncoder，视频编码VideoEncoder功能

--- app/src/main/java/com/example/advd/audioandvideolearn_demo_master/base_05/AudioEncoder.java
+++ app/src/main/java/com/example/advd/audioandvideolearn_demo_master/base_05/AudioEncoder.java
@@ -2,6 +2,7 @@ package com.example.advd.audioandvideolearn_demo_master.base_05;
 
 import android.annotation.TargetApi;
 import android.content.Context;
+import android.content.IntentFilter;
 import android.media.AudioFormat;
 import android.media.AudioRecord;
 import android.media.MediaCodec;
@@ -15,6 +16,7 @@ import android.util.Log;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.ByteBuffer;
 
 
 /**
@@ -23,13 +25,14 @@ import java.io.IOException;
  * desc: 音频编码类
  */
 public class AudioEncoder implements Runnable {
+    private static final String TAG = "AudioEncoder";
 
     private AudioRecord mRecord;
     private MediaCodec mEncoder;
 
     // 录音设置
     private String mime = MediaFormat.MIMETYPE_AUDIO_AAC; // aac
-    private int rate = 256000; // 采样率
+    private int rate = 256000; //波特率，256kb
 
     private int sampleRate = 44100; //采样率，默认44.1k
     private int channelCount = 2;  //音频采样通道，默认2通道
@@ -37,20 +40,19 @@ public class AudioEncoder implements Runnable {
     private int audioFormat = AudioFormat.ENCODING_PCM_16BIT; //设置采样数据格式，默认16比特PCM
 
     private int bufferSize;
-    private byte[] buffer;
     private Thread mThread;
     private boolean isRecording;
 
     private String mSaveName;
     private FileOutputStream fos;
 
-
     @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
-    public void prepare(Context context) throws IOException {
+    public void prepare( ) throws IOException {
         // init Codec
         MediaFormat format = MediaFormat.createAudioFormat(mime, sampleRate, channelCount);
-        format.setInteger(MediaFormat.KEY_AAC_PROFILE, MediaCodecInfo.CodecProfileLevel.AACObjectLC);
         format.setInteger(MediaFormat.KEY_BIT_RATE, rate);
+        format.setInteger(MediaFormat.KEY_AAC_PROFILE,
+                MediaCodecInfo.CodecProfileLevel.AACObjectLC);
         mEncoder = MediaCodec.createEncoderByType(mime);
         mEncoder.configure(format, null, null, MediaCodec.CONFIGURE_FLAG_ENCODE); //设置为编码器
 
@@ -62,10 +64,9 @@ public class AudioEncoder implements Runnable {
                 audioFormat,// 采样编码 一般是pcm编码16位
                 bufferSize); // 缓冲区数据大小
 
-        buffer = new byte[bufferSize];
-        File f = new File(context.getExternalFilesDir(Environment.DIRECTORY_MUSIC), System.currentTimeMillis() + mSaveName);
+        File f = new File( System.currentTimeMillis() + mSaveName);
         if (!f.mkdir()) {
-            Log.i("", "createAudioRecord:   Directory not created");
+            Log.i(TAG, "createAudioRecord:   Directory not created");
         }
         if (f.exists()) {
             f.delete();
@@ -84,7 +85,7 @@ public class AudioEncoder implements Runnable {
         }
 
         isRecording = true;
-        mThread = new Thread();
+        mThread = new Thread(this);
         mThread.start();
     }
 
@@ -103,12 +104,107 @@ public class AudioEncoder implements Runnable {
         fos.close();
     }
 
+
     @Override
     public void run() {
-
         while (isRecording) {
-
-
+            try {
+                readOutputData();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
+    }
+
+    private ByteBuffer getInputBuffer(int index) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            return mEncoder.getInputBuffer(index);
+        } else {
+            return mEncoder.getInputBuffers()[index];
+        }
+    }
+
+    private ByteBuffer getOutputBuffer(int index) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            return mEncoder.getOutputBuffer(index);
+        } else {
+            return mEncoder.getOutputBuffers()[index];
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+    private void readOutputData() throws IOException {
+        int inIndex = mEncoder.dequeueInputBuffer(-1);
+        Log.d(TAG, " inIndex:" + inIndex);
+        if (inIndex >= 0) {
+            ByteBuffer buffer = getInputBuffer(inIndex);
+            buffer.clear();
+            int length = mRecord.read(buffer, bufferSize);
+            Log.d(TAG, " length: " + length);
+            if (length > 0) {
+                mEncoder.queueInputBuffer(inIndex, 0,
+                        length, System.nanoTime() / 1000, 0);
+            }
+        }
+
+        MediaCodec.BufferInfo info = new MediaCodec.BufferInfo();
+        int outIndex;
+        do {
+            outIndex = mEncoder.dequeueOutputBuffer(info, 0);
+            Log.d(TAG, " outIndex: " + outIndex);
+            if (outIndex >= 0) {
+                ByteBuffer buffer = getOutputBuffer(outIndex);
+                buffer.position(info.offset);
+
+                // 添加aac头部，buffer数据设置到temp
+                byte[] temp = new byte[info.size + 7];
+                buffer.get(temp, 7, info.size);
+                addADTStoPacket(temp, temp.length);
+
+                fos.write(temp);
+                mEncoder.releaseOutputBuffer(outIndex, false);
+            }
+        } while (outIndex >= 0);
+    }
+
+    /**
+     * 给编码出的aac裸流添加adts头字段
+     *
+     * @param packet    要空出前7个字节，否则会搞乱数据
+     * @param packetLen
+     */
+    private void addADTStoPacket(byte[] packet, int packetLen) {
+        int profile = 2;  //AAC LC
+        int freqIdx = 4;  //44.1KHz
+        int chanCfg = 2;  //CPE
+        packet[0] = (byte) 0xFF;
+        packet[1] = (byte) 0xF9;
+        packet[2] = (byte) (((profile - 1) << 6) + (freqIdx << 2) + (chanCfg >> 2));
+        packet[3] = (byte) (((chanCfg & 3) << 6) + (packetLen >> 11));
+        packet[4] = (byte) ((packetLen & 0x7FF) >> 3);
+        packet[5] = (byte) (((packetLen & 7) << 5) + 0x1F);
+        packet[6] = (byte) 0xFC;
+    }
+
+    // ############################################
+
+    public void setMime(String mime) {
+        this.mime = mime;
+    }
+
+    public void setRate(int rate) {
+        this.rate = rate;
+    }
+
+    public void setSaveName(String saveName) {
+        mSaveName = saveName;
+    }
+
+    public void setSampleRate(int sampleRate) {
+        this.sampleRate = sampleRate;
+    }
+
+    public void setChannelCount(int channelCount) {
+        this.channelCount = channelCount;
     }
 }
--- app/src/main/java/com/example/advd/audioandvideolearn_demo_master/base_05/VideoEncoder.java
+++ app/src/main/java/com/example/advd/audioandvideolearn_demo_master/base_05/VideoEncoder.java
@@ -1,13 +1,243 @@
 package com.example.advd.audioandvideolearn_demo_master.base_05;
 
+import android.annotation.TargetApi;
+import android.media.MediaCodec;
+import android.media.MediaCodecInfo;
+import android.media.MediaFormat;
+import android.os.Build;
+import android.util.Log;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+
 /**
  * author: ycl
  * date: 2018-12-17 0:34
  * desc:
  */
 public class VideoEncoder implements Runnable {
+    private static final String TAG = "VideoEncoder";
+
+    private MediaCodec mEncoder;
+
+    private String mime = MediaFormat.MIMETYPE_VIDEO_AVC; // avc
+    private int rate = 256000; //波特率，256kb
+    private int frameRate = 24;  //帧率，24帧
+    private int frameInterval = 1; //关键帧一秒一关键帧
+
+    private int fpsTime;
+    private boolean isRecording;
+    private Thread mThread;
+    private int width;
+    private int height;
+
+    private FileOutputStream fos;
+    private String mSaveName;
+
+
+    // 外部写入一帧数据进来
+    private byte[] nowFeedData;
+    private long nowTimeStep;
+    private boolean hasNewData = false;
+
+    private byte[] yuv;
+    private byte[] mHeadInfo = null;
+
+
+    public VideoEncoder() {
+        fpsTime = 1000 / frameRate;
+    }
+
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+    public void prepare(int width, int height) throws IOException {
+        this.width = width;
+        this.height = height;
+
+        // init Codec
+        MediaFormat format = MediaFormat.createVideoFormat(mime, width, height);
+        format.setInteger(MediaFormat.KEY_BIT_RATE, rate);
+        format.setInteger(MediaFormat.KEY_FRAME_RATE, frameRate);
+        format.setInteger(MediaFormat.KEY_I_FRAME_INTERVAL, frameInterval);
+        format.setInteger(MediaFormat.KEY_COLOR_FORMAT,
+                MediaCodecInfo.CodecCapabilities.COLOR_FormatYUV420Planar);
+        mEncoder = MediaCodec.createEncoderByType(mime);
+        mEncoder.configure(format, null, null, MediaCodec.CONFIGURE_FLAG_ENCODE); //设置为编码器
+
+        File f = new File(System.currentTimeMillis() + mSaveName);
+        if (!f.mkdir()) {
+            Log.i(TAG, "createAudioRecord:   Directory not created");
+        }
+        if (f.exists()) {
+            f.delete();
+        }
+        fos = new FileOutputStream(f);
+    }
+
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+    public void start() throws InterruptedException {
+        mEncoder.start();
+
+        if (mThread != null && mThread.isAlive()) {
+            isRecording = false;
+            mThread.join();
+        }
+
+        isRecording = true;
+        mThread = new Thread(this);
+        mThread.start();
+    }
+
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+    public void stop() throws InterruptedException, IOException {
+        mThread.join();
+
+        isRecording = false;
+
+        mEncoder.stop();
+        mEncoder.release();
+
+        fos.flush();
+        fos.close();
+    }
+
+
     @Override
     public void run() {
+        while (isRecording) {
+            long time = System.currentTimeMillis();
 
+            if (nowFeedData != null) {
+                try {
+                    readOutputData(nowFeedData, nowTimeStep);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            long lt = System.currentTimeMillis() - time;
+
+            if (fpsTime > lt) { // 控制fps准时
+                try {
+                    Thread.sleep(fpsTime - lt);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+
+    private ByteBuffer getInputBuffer(int index) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            return mEncoder.getInputBuffer(index);
+        } else {
+            return mEncoder.getInputBuffers()[index];
+        }
+    }
+
+    private ByteBuffer getOutputBuffer(int index) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            return mEncoder.getOutputBuffer(index);
+        } else {
+            return mEncoder.getOutputBuffers()[index];
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+    private void readOutputData(byte[] data, long timeStep) throws IOException {
+        int inIndex = mEncoder.dequeueInputBuffer(-1);
+        Log.d(TAG, " inIndex:" + inIndex);
+        if (inIndex >= 0) {
+            if (hasNewData) {
+                if (yuv == null) {
+                    yuv = new byte[width * height * 3 / 2];
+                }
+                // 外部传入的数据都需要转换为YUV数据,才能进行后续的操作
+                rgbaToYuv(data, width, height, yuv);
+            }
+            ByteBuffer buffer = getInputBuffer(inIndex);
+            buffer.clear();
+            buffer.put(yuv);
+            mEncoder.queueInputBuffer(inIndex, 0, yuv.length, timeStep, 0);
+        }
+
+        MediaCodec.BufferInfo info = new MediaCodec.BufferInfo();
+        int outIndex = mEncoder.dequeueOutputBuffer(info, 0);
+        while (outIndex >= 0) {
+            Log.d(TAG, " outIndex: " + outIndex);
+            ByteBuffer buffer = getOutputBuffer(outIndex);
+
+            // 获取临时数据
+            byte[] temp = new byte[info.size];
+            buffer.get(temp);
+            if (info.flags == MediaCodec.BUFFER_FLAG_CODEC_CONFIG) { // 不是媒体数据
+                Log.e(TAG, "start frame");
+                mHeadInfo = new byte[info.size];
+                mHeadInfo = temp;
+            } else if (info.flags % 8 == MediaCodec.BUFFER_FLAG_KEY_FRAME) {
+                Log.d(TAG, "info.flags ->" + info.flags);
+                Log.e(TAG, "key frame");
+
+                // 把头部数据，关键帧数据copy到keyframe 里面去
+                byte[] keyframe = new byte[temp.length + mHeadInfo.length];
+                System.arraycopy(mHeadInfo, 0, keyframe, 0, mHeadInfo.length);
+                System.arraycopy(temp, 0, keyframe, mHeadInfo.length, temp.length);
+                fos.write(keyframe, 0, keyframe.length);
+            } else if (info.flags == MediaCodec.BUFFER_FLAG_END_OF_STREAM) {
+                Log.e(TAG, "end frame");
+            } else {
+                fos.write(temp, 0, temp.length);
+            }
+
+            mEncoder.releaseOutputBuffer(outIndex, false);
+            outIndex = mEncoder.dequeueOutputBuffer(info, 0);
+        }
+    }
+
+
+    private void rgbaToYuv(byte[] rgba, int width, int height, byte[] yuv) {
+        final int frameSize = width * height;
+
+        int yIndex = 0;
+        int uIndex = frameSize;
+        int vIndex = frameSize + frameSize / 4;
+
+        int R, G, B, Y, U, V;
+        int index = 0;
+        for (int j = 0; j < height; j++) {
+            for (int i = 0; i < width; i++) {
+                index = j * width + i;
+                if (rgba[index * 4] > 127 || rgba[index * 4] < -128) {
+                    Log.e(TAG, "-->" + rgba[index * 4]);
+                }
+                R = rgba[index * 4] & 0xFF;
+                G = rgba[index * 4 + 1] & 0xFF;
+                B = rgba[index * 4 + 2] & 0xFF;
+
+                Y = ((66 * R + 129 * G + 25 * B + 128) >> 8) + 16;
+                U = ((-38 * R - 74 * G + 112 * B + 128) >> 8) + 128;
+                V = ((112 * R - 94 * G - 18 * B + 128) >> 8) + 128;
+
+                yuv[yIndex++] = (byte) ((Y < 0) ? 0 : ((Y > 255) ? 255 : Y));
+                if (j % 2 == 0 && index % 2 == 0) {
+                    yuv[uIndex++] = (byte) ((U < 0) ? 0 : ((U > 255) ? 255 : U));
+                    yuv[vIndex++] = (byte) ((V < 0) ? 0 : ((V > 255) ? 255 : V));
+                }
+            }
+        }
+    }
+
+    /**
+     * 由外部写入一帧数据
+     *
+     * @param data
+     * @param timeStep
+     */
+    public void feedData(final byte[] data, final long timeStep) {
+        hasNewData = true;
+        nowFeedData = data;
+        nowTimeStep = timeStep;
     }
 }
